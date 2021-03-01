9e97e7dabbd36ad355abea93a8687ee47f567993
TAMGNO LEONEL <virgiltako@gmail.com>
2021-03-01 04:25:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express= require('express');
 const app = express();
 const port =process.env.PORT || 3000;
+
+var student= [
+    {id : 1, name : "student 1" },
+    {id : 1, name : "student 2" },
+    {id : 1, name : "student 3" },
+]
+
+app.get('/api/student',(req,res)=>{
+    res.send(student);
+})
+
 app.listen(port,()=> console.log(`server running on ${port}...`));
